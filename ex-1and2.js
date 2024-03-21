992af250d49d083a7de63a14a168b3f720ad210e
KPBoom <163835174+KPBoom@users.noreply.github.com>
2024-03-21 18:58:27 +0700

    Update ex-1and2.js

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,11 @@
 //Exercise #1: Variable Creation
 //Start coding here
-
+let name = "Karter";
+let surname = "Anderson";
+let address = "54/24 Gp5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+const foodShopName = "Chef's Table by Chef James";
+const totalPrice = 15000;
+const shopBranch = 1500;
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
